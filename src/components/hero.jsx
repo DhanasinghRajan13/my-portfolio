6a501d0f48f883e58b0f9cafa7c46f3acd8a4dd8
DhanasinghRajan13 <dhanasinghrajan182113@gmail.com>
2026-03-21 00:58:59 +0530

    updated todo app UI

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,44 +4,49 @@ import "./style.css";
 function Hero() {
   return (
     <section className="hero">
-      <div className="hero-main-nav">
-        <div className="hero-nav">
-          <nav className="hero-nav-links">
-            <ul>
-              <li>
-                <a href="#">About me</a>
-              </li>
-              <li>
-                <a href="#">Skills</a>
-              </li>
-              <li>
-                <a href="#">Project</a>
-              </li>
-              <li>
-                <a href="#">portfolio</a>
-              </li>
-            </ul>
-            <button>Contact me</button>
-          </nav>
+      <div className="hero-center">
+        <h2>my name is Dhanasingh</h2>
+        <h3>I'M A DEVELOPER</h3>
+
+        <div className="vertical-line"></div>
+
+        <div className="icons">
+          <img src="src/assets/at.png" width="40px" height="40px"/>
+          <img src="src/assets/github.png" width="40px" height="40px"/>
+          <img src="src/assets/linkedin.png" width="40px" height="40px" />
         </div>
       </div>
 
       <div className="hero-main-content">
         <div className="hero-content">
-          <h3>Hi,I am</h3><br/>
+          <h3>Hi,I am</h3>
           <h1>Dhanasingh R</h1>
           <h2>Frontend Developer, React Developer</h2>
 
           <div className="hero-button">
-            <br/>
-            <button >
-              <a  href="mailto:dhanasinghrajan007@gmail.com" target="_blank"><img src="src/assets/at.png" /></a>
+            <br />
+            <button>
+              <a href="mailto:dhanasinghrajan007@gmail.com" target="_blank">
+                <img src="src/assets/at.png" />
+              </a>
             </button>
             <button>
-              <a href="https:github.com/DhanasinghRajan13 "  target="_blank" rel="noopener noreferrer"><img src="src/assets/github.png" /></a>
+              <a
+                href="https://github.com/DhanasinghRajan13 "
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="src/assets/github.png" />
+              </a>
             </button>
             <button>
-              <a href="https:www.linkedin.com/in/dhanasinghrajan" target="_blank" rel="noopener noreferrer"><img src="src/assets/linkedin.png" /></a>
+              <a
+                href="https://www.linkedin.com/in/dhanasinghrajan"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="src/assets/linkedin.png" />
+              </a>
             </button>
           </div>
         </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <nav className="navbar">
+    <>
+      <nav className="navbar">
         <h1>DSR</h1>
-      <ul>
-        <li><a href="#">About me</a></li>
-        <li><a href="#">Skills</a></li>
-        <li><a href="#">Project</a></li>
-        <li><a href="#">portfolio</a></li>
-      </ul>
-      <button>Contact me</button>
-    </nav>
+
+        <ul className="nav-links">
+          <li>
+            <a href="#description">About me</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#project">Project</a>
+          </li>
+          <li>
+            <a href="#hero">portfolio</a>
+          </li>
+        </ul>
+        <button>Contact me</button>
+
+        {/* <div className="hamburger" onClick={() => setOpen(true)}>
+          ☰
+        </div> */}
+      </nav>
+      <div className="hamburger" onClick={() => setOpen(true)}>
+          ☰
+        </div>
+
+      {open && (
+        <div className="mobile-menu">
+          <span className="close" onClick={() => setOpen(false)}>
+            ✕
+          </span>
+
+          <ul className="menu-links">
+            <li>
+              <a href="#description" onClick={() => setOpen(false)}>About me</a>
+            </li>
+            
+            <li>
+              <a href="#skills" onClick={() => setOpen(false)}>Skills</a>
+            </li>
+            <li>
+              <a href="#project" onClick={() => setOpen(false)}>Project</a>
+            </li>
+            <li>
+              <a href="#hero" onClick={() => setOpen(false)}>portfolio</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
 
